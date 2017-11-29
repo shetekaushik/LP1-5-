410234cfe29519ba000cf65988996e700e648dac
priyankakadu2394@gmail.com <priyankakadu2394@gmail.com>
2017-11-29 17:27:58 +0530

    Third Version

--- Stat.R
+++ Stat.R
@@ -24,3 +24,36 @@ min_max<-function(data,user,new_min,new_max){
 decimal<-function(user,scale1){
   (user/scale1)
 }
+data<-read.csv(file="Housing.csv",head=TRUE,sep=",")
+
+varience<-function(data){
+  sqrt(sd(data))
+}
+
+range1<-function(data){
+  max(data)-min(data)
+}
+
+get_mode<-function(data){
+  uniq<-unique(data)
+  uniq[which.max(tabulate(match(data,uniq)))]
+}
+
+z_score<-function(data,user){
+  (user-min(data))/sd(data)
+}
+
+min_max<-function(data,user,new_min,new_max){
+  ((user-min(data))/(max(data)-min(data)))*(new_max-new_min)+new_min
+}
+
+decimal<-function(user,scale1){
+  (user/scale1)
+}
+
+
+HistGram<-function(data,breaks,X_min_lim,X_max_lim,Y_min_lim,X_max_lim){
+
+hist.default(data,breaks,xlim = c(X_min_lim,X_max_lim),ylim = c(Y_min_lim,Y_max_lim))
+}
+
